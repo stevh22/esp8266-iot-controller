b8224d9b5fa22191de7987afa8a9fa8cde74019b
Steven <stevh22@gmail.com>
2019-08-21 12:13:03 +0200

    Cleanup old codesnippet

--- SimpleMenu.cpp
+++ SimpleMenu.cpp
@@ -13,44 +13,6 @@ void SimpleMenu::SetData(char* data){
   return data;
 }*/
 
-/*void ShowMenu(String MenuArray[],int MenuSize){
-  display.clearDisplay();
-  display.setCursor(0,0);
-  int maxLines = (SSD1306_LCDHEIGHT / (MENU_FONT_SIZE * 8))-1;
-  MENU_SIZE = MenuSize-1;
-  
-  // Draw Titlebar
-  display.setTextSize(MENU_FONT_SIZE);
-  display.setTextColor(TITLE_COLOR);
-  display.println(MenuArray[0]);
-
-  // Draw Options
-  display.setTextColor(TEXT_COLOR);
-  for(int i=1;i<=maxLines;i++){
-    if(i+DISPLAY_OFFSET > MENU_SIZE){break;}
-    if(i+DISPLAY_OFFSET == CURSOR_POS){ display.print("> "); }else{ display.print("  "); }
-    display.println(MenuArray[i+DISPLAY_OFFSET]);
-  }
-  display.display();
-}*/
-
-/*void ShowEntryInfo(String EntryTitle, String EntryInfo){
-  display.clearDisplay();
-  display.setCursor(0,0);
-  int maxLines = (SSD1306_LCDHEIGHT / (MENU_FONT_SIZE * 8))-1;
-  
-  // Draw Titlebar
-  display.setTextSize(MENU_FONT_SIZE);
-  display.setTextColor(TITLE_COLOR);
-  display.print(EntryTitle);
-  display.println(":");
-
-  // Draw Options
-  display.setTextColor(TEXT_COLOR);
-  display.print(EntryInfo);
-  display.display();
-}*/
-
 bool SimpleMenu::isMenuShown(){
   return _menuShown;  
 }
